645a236cb87933d637bc34e031b2a5e240e4627c
chae1-1111 <dla4212@naver.com>
2023-05-17 02:31:37 +0900

    chore : celenium test:

--- selenium_test.py
+++ selenium_test.py
@@ -3,11 +3,12 @@ from selenium.webdriver.common.by import By
 
 URL = "https://media.naver.com/press"
 
-driver = webdriver.Chrome(executable_path='chromedriver')
+driver = webdriver.Chrome(executable_path="chromedriver")
 
 driver.get(URL)
 
 pressElements = driver.find_elements(By.CSS_SELECTOR, "ul.press_list_only_logo li a")
+
 
 def getPress(pressElement):
     pressName = pressElement.find_element(By.TAG_NAME, "img").get_attribute("alt")
@@ -16,6 +17,15 @@ def getPress(pressElement):
     return {"name": pressName, "code": pressCode}
 
 
-
 presses = list(map(getPress, pressElements))
 
+# for press in presses:
+press = presses[0]
+newDriver = webdriver.Chrome(executable_path="chromedriver")
+newDriver.get(URL + "/" + press["code"] + "?sid=100")
+
+print(URL + "/" + press["code"] + "?sid=100")
+elements = newDriver.find_elements(By.CSS_SELECTOR, "span.press_edit_news_title")
+
+for element in elements:
+    print(element.text)
